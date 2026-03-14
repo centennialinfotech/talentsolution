cde03a8fa17155c8002fe061393f5328692c20f6
cry-wizard <sidd160306@gmail.com>
2026-03-14 08:22:43 +0530

    FIxing Backend api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ci-indeed-jp-new.onrender.com/api',
+  baseURL: 'https://talentsolution.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
